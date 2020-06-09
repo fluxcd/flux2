43876b5ab99947a55cfc87cf157f2323aeef0134
Hidde Beydals <hello@hidde.co>
2020-06-09 11:16:56 +0200

    Make SSH host key utilities publicly accessible

--- pkg/ssh/host_key.go
+++ pkg/ssh/host_key.go
@@ -14,10 +14,10 @@ import (
 // Any errors (e.g. authentication  failures) are ignored, except if
 // no key could be collected from the host.
 func ScanHostKey(host string, timeout time.Duration) ([]byte, error) {
-	col := &collector{}
+	col := &HostKeyCollector{}
 	config := &ssh.ClientConfig{
-		HostKeyCallback:   col.StoreKey(),
-		Timeout:           timeout,
+		HostKeyCallback: col.StoreKey(),
+		Timeout:         timeout,
 	}
 	client, err := ssh.Dial("tcp", host, config)
 	if err == nil {
@@ -29,7 +29,9 @@ func ScanHostKey(host string, timeout time.Duration) ([]byte, error) {
 	return col.knownKeys, err
 }
 
-type collector struct {
+// HostKeyCollector offers a StoreKey method which provides an
+// HostKeyCallBack to collect public keys from an SSH server.
+type HostKeyCollector struct {
 	knownKeys []byte
 }
 
@@ -37,7 +39,7 @@ type collector struct {
 // To collect multiple public key types from the host, multiple
 // SSH dials need with the ClientConfig HostKeyAlgorithms set to
 // the algorithm you want to collect.
-func (c *collector) StoreKey() ssh.HostKeyCallback {
+func (c *HostKeyCollector) StoreKey() ssh.HostKeyCallback {
 	return func(hostname string, remote net.Addr, key ssh.PublicKey) error {
 		c.knownKeys = append(
 			c.knownKeys,
@@ -45,4 +47,9 @@ func (c *collector) StoreKey() ssh.HostKeyCallback {
 		)
 		return nil
 	}
+}
+
+// GetKnownKeys returns the collected public keys in bytes.
+func (c *HostKeyCollector) GetKnownKeys() []byte {
+	return c.knownKeys
 }
