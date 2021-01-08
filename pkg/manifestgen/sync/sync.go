daeb41c31b647ac9b9476b0a1dada917bb89020d
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2021-01-08 11:01:40 +0100

    Uses path instead of filepath
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- pkg/manifestgen/sync/sync.go
+++ pkg/manifestgen/sync/sync.go
@@ -19,7 +19,7 @@ package sync
 import (
 	"bytes"
 	"fmt"
-	"path/filepath"
+	"path"
 	"strings"
 	"time"
 
@@ -93,7 +93,7 @@ func Generate(options Options) (*manifestgen.Manifest, error) {
 	}
 
 	return &manifestgen.Manifest{
-		Path:    filepath.Join(options.TargetPath, options.Namespace, options.ManifestFile),
+		Path:    path.Join(options.TargetPath, options.Namespace, options.ManifestFile),
 		Content: fmt.Sprintf("---\n%s---\n%s", resourceToString(gitData), resourceToString(ksData)),
 	}, nil
 }
