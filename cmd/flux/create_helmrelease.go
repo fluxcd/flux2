64f39e160b79b199710620c074752e7e3c6affd5
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2021-09-21 09:34:12 +0100

    Check if helmRelease.Spec.Install is nil
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- cmd/flux/create_helmrelease.go
+++ cmd/flux/create_helmrelease.go
@@ -197,6 +197,10 @@ func createHelmReleaseCmdRun(cmd *cobra.Command, args []string) error {
 	}
 
 	if helmReleaseArgs.crds != "" {
+		if helmRelease.Spec.Install == nil {
+			helmRelease.Spec.Install = &helmv2.Install{}
+		}
+
 		helmRelease.Spec.Install.CRDs = helmv2.Create
 		helmRelease.Spec.Upgrade = &helmv2.Upgrade{CRDs: helmv2.CRDsPolicy(helmReleaseArgs.crds.String())}
 	}
